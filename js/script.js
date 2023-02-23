a83bdf4e89a0f970c3e1896068273ca68a35d9b8
Your Name~ <you@example.com>
2023-02-23 13:32:20 +0700

    upload website kedai kopi

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//toggle clas active
+const navbarNav = document.querySelector
+('.navbar-nav');
+//ketika humburger menu di klik
+
+document.querySelector('#hamburger-menu').
+onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+//klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector
+('#hamburger-menu');
+document.addEventListener('click', function(e){
+     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+     }
+}) 
